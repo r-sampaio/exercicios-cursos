0f94d51024484e34c291e8af67325aa898f7c40e
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-11 23:54:28 -0300

    update

---
+++ Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_020.py
@@ -0,0 +1,4 @@
+'''
+O mesmo professor do desafio anterios quer sortear a ordem de apresentação de trabalhos dos alunos.
+Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+'''
