37433af81142947bc295c6c2ce6dcf836b95c090
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-16 15:08:03 -0300

    update

--- Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_020.py
+++ Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_020.py
@@ -2,3 +2,16 @@
 O mesmo professor do desafio anterios quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 '''
+from math import shuffle
+import os
+
+os.system('cls')
+primeiro = str(input('Qual o nome do primeiro aluno: '))
+segundo = str(input('Qual o nome do segundo aluno: '))
+terceiro = str(input('Qual o nome do terceiro aluno: '))
+quarto = str(input('Qual o nome do quarto aluno: '))
+# winner = shuffle([primeiro, segundo, terceiro, quarto])
+
+winner = [primeiro, segundo, terceiro, quarto]
+shuffle(winner)
+print(winner)
--- Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_021.py
+++ Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_021.py
@@ -1,3 +1,8 @@
 '''
 Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 '''
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('desafio_021.mp3')
+mixer.music.play()
