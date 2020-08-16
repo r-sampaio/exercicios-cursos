72fc5aa43b309e258f8344ab5dee6c656632a0e7
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-16 12:03:27 -0300

    update

--- Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_019.py
+++ Curso em Video - Python/Mundo-01/Curso Python #8 - Utilizando Módulos/desafio_019.py
@@ -2,3 +2,17 @@
 Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 '''
+from random import choice
+import os
+
+os.system('cls')
+primeiro = str(input('Qual o nome do primeiro aluno: '))
+segundo = str(input('Qual o nome do segundo aluno: '))
+terceiro = str(input('Qual o nome do terceiro aluno: '))
+quarto = str(input('Qual o nome do quarto aluno: '))
+
+# lista = [primeiro, segundo, terceiro, quarto]
+# winner = choice(lista)
+
+winner = choice([primeiro, segundo, terceiro, quarto])
+print(f'{winner}')
