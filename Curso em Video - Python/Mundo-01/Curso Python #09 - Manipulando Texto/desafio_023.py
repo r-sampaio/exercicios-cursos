e921d72a3f8aed199ef6dc3713a505d816b88431
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-17 00:39:52 -0300

    update

--- Curso em Video - Python/Mundo-01/Curso Python #09 - Manipulando Texto/desafio_023.py
+++ Curso em Video - Python/Mundo-01/Curso Python #09 - Manipulando Texto/desafio_023.py
@@ -1,2 +1,7 @@
 # Faça um programa que leia de 0 a 9999 e mostra na tela cada um dos digitos separados.
-    # * Ex: dig
+    # * Ex: Digite um número: 1834
+        # * unidade: 4
+        # * dezena: 3
+        # * centena: 8
+        # * milhar: 1
+
