84524a008243d8248e8318544ab378c1d79c90de
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-19 23:25:25 -0300

    update

--- Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_039.py
+++ Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_039.py
@@ -4,3 +4,20 @@
     # - Se já passou do tempo do alistamento.
     # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
+from datetime import date
+
+sexo = str(input('Digite seu sexo [M/F]: ')).upper().strip()
+if sexo == 'M':
+    nascimento = int(input('Qual o ano de seu nascimento? '))
+    ano = date.today().year
+    result = ano - nascimento
+    difer = result - 18
+    dever = ano - difer
+    if result < 18:
+        print(f'Ainda tem tempo, voce so tem {result} anos')
+    elif result == 18:
+        print(f'Esta na hora, você completa {result} este ano {ano}')
+    elif result > 18:
+        print(f'Ja passou da hora, você esta {difer} anos atrasado, deveria ter se alistado em {dever}')
+else:
+    print(f'Você não precisa se alistar')
--- Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_041.py
+++ Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_041.py
@@ -6,3 +6,18 @@
     # - Até 20 anos: SÊNIOR
     # - Acima: MASTER
 
+from datetime import date
+ano = date.today().year
+data = int(input('Digite o ano que você nasceu? '))
+idade = ano - data
+if idade <= 9:
+    print('MIRIM')
+elif idade > 9 and idade <= 14:
+    print('INFANTIL')
+elif idade > 14 and idade <= 19:
+    print('JUNIOR')
+elif idade > 19 and idade <= 20:
+    print('SÊNIOR')
+elif idade > 20:
+    print('MASTER')
+print(idade)
