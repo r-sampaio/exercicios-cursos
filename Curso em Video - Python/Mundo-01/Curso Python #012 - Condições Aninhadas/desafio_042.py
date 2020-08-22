1f541936bf99c28517f628e350bcae87e5d51861
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-21 23:54:34 -0300

    Update desafio_042.py

--- Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_042.py
+++ Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_042.py
@@ -9,6 +9,14 @@ r2 = float(input('Digite o tamanho de R2: '))
 r3 = float(input('Digite o tamanho de R3: '))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Forma triangulo')
+    print('Forma um triângulo')
+    if r1 != r2 and r2 != r3 and r3 != r1:
+        print('Triângulo Escaleno')
+    elif r1 == r2 !=r3 or r2 == r3 != r1 or r3 == r1 != r2:
+        print('Triângulo Isósceles')
+    elif r1 == r2 and r2 == r3 and r3 == r1:
+        print('Triângulo Equilátero')
+#    else:
+#        print('Triângulo Isósceles')
 else:
-    print('Nao forma triangulo')
+    print('Não forma um triângulo')
