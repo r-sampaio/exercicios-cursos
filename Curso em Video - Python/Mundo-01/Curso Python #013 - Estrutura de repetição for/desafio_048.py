643e7117a4ddf65082972ec42b57cdf63f7381dc
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-22 19:07:53 -0300

    update

---
+++ Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_048.py
@@ -0,0 +1,6 @@
+# Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três
+    # e que se encontram no intervalo de 1 até 500.
+s = 0
+for i in range(1, 500, 2):
+    s += i
+    print(s)
---
+++ Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_053.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, deconsederando os espaços.
+    # EX: (SEM ACENTOS)
+    # APOS A SOPA                   APOSASOPA
+    # A SACADA DA CASA              ASACADADACASA
+    # A TORRE DA DERROTA            ATORREDADERROTA
+    # O LOBO AMA O BOLO             OLOBOAMAOBOLO
+    # ANOTARAM A DATA DA MARATONA   ANOTARAMADATADAMARATONA
+
