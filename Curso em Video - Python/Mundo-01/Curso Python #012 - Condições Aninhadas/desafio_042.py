74f175317e094c751755f1167107f6f883182a35
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-21 23:41:04 -0300

    Update desafio_042.py

--- Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_042.py
+++ Curso em Video - Python/Mundo-01/Curso Python #012 - Condições Aninhadas/desafio_042.py
@@ -4,3 +4,11 @@
     # - Isósceles: dois lados iguais
     # - Escaleno: todos os lados diferantes
 
+r1 = float(input('Digite o tamanho de R1: '))
+r2 = float(input('Digite o tamanho de R2: '))
+r3 = float(input('Digite o tamanho de R3: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Forma triangulo')
+else:
+    print('Nao forma triangulo')
