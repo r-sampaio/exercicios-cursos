23f9480436bc8d35bf5338699b2812a535aa6966
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 14:00:15 -0300

    update

--- Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_058.py
+++ Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_058.py
@@ -1,3 +1,11 @@
 # Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10.
-    # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final qunatos 
+    # Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos 
     # palpites foram necessários para vencer.
+
+from  random import randint
+
+p = 0
+c = randint(0, 10)
+u = int(input('Digite um número entre 0 e 10: '))
+while c != u:
+    
