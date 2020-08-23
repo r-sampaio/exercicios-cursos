a355270cc5336ec45b4341b456fe253698470abf
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 19:03:45 -0300

    Update desafio_063.py

--- Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_063.py
+++ Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_063.py
@@ -2,8 +2,14 @@
     # de uma Sequência de Fibonacci.
     # Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
 
-
+s = 0
 n = 0
+n1 = 0
+n2 = 1
 termos = int(input('Digite quantos termos exibir: '))
-while n < termos:
-    print(f'{n}', end='')
+while n <= termos:
+    print(f'{s} ', end='')
+    s = n1 + n2
+    n1 = n2
+    n2 = s
+    termos -= 1
