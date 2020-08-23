3e6e545cbbce0f1ae47224de94fdb794a93b11fb
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 18:16:54 -0300

    Update desafio_062.py

--- Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_062.py
+++ Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_062.py
@@ -1,7 +1,7 @@
 # melhore o DESAFIO 061, perguntando para o usuário se ele quer mostrar mais alguns termos. 
     # O programa encerra quando ele disser que quer mostrar 0 termos.
 
-
+t = 0
 n = 0
 termo = int(input('Digite o termo: '))
 razao = int(input('Digite a razao: '))
@@ -9,9 +9,11 @@ total = 0
 m = 10
 while m != 0:
     total += m
-    while n <= total:
+    t += m
+    while n < total:
         n += 1
         print(f'{termo}', end='')
-        print(', ' if n < m else '. ', end='')
+        print(', ' if n < t else '. ', end='')
         termo += razao
-m = int(input('Deseja mostra mais quantos numeros? '))
+    m = int(input('\nDeseja mostra mais quantos numeros? '))
+print(f'Foram mostrados {n-1} valores.')
