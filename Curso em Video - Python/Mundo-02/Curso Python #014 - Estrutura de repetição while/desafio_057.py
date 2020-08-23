3113e6f72204be87f5dc06e436065f6d2445dbb0
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 12:42:20 -0300

    Update desafio_057.py

--- Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_057.py
+++ Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_057.py
@@ -1,2 +1,2 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os calores 'M' ou 'F'. Caso esteja errado,
-    # peá a digitação novamente até ter um valor correto.
+    # peça a digitação novamente até ter um valor correto.
