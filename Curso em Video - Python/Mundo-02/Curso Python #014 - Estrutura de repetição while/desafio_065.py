5ee47dc9e2ac7881801ce15f2254314ced5616c7
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 19:51:12 -0300

    Update desafio_065.py

--- Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_065.py
+++ Curso em Video - Python/Mundo-02/Curso Python #014 - Estrutura de repetição while/desafio_065.py
@@ -1,3 +1,20 @@
 # Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre
     # todos os valores e qual foi o maior e o menor valor lido. o programa deve perguntar ao usuário se ele 
     # quer ou não continuar a digitar valores.
+
+
+n = 0
+q = 1
+o = 'S'
+
+while o in 'S':
+    n = float(input('Digite um número: '))
+    o = str(input('Quer continuar? ')).upper().strip()[0]
+    n += n
+    q += 1
+    if n <= n:
+        maior = n
+    elif n >= n:
+        menor = n
+s = n / q
+print(f'{s} Media e o maior foi {maior} e o menor foi {menor}.')
