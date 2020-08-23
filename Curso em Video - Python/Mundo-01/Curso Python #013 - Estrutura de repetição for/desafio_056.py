43e7f158cedd5b984d5fb4d38dfacba29b69f32f
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-23 00:15:48 -0300

    update

--- Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_056.py
+++ Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_056.py
@@ -2,3 +2,28 @@
     # A média de idade do grupo.
     # Qual é o nome do homem mais velho.
     # Quantas mulheres têm menos de 20 anos.
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = 0
+totalmulher = 0
+
+for i in range(1, 5):
+    print(f'{i} pessoa')
+    nome = str(input('Nome: ')).upper()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).upper()
+    somaidade += idade
+    if i == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    elif sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    elif sexo in 'Ff' and idade > 20:
+        totalmulher += 1
+mediaidade = somaidade / 4
+print(f'Media de idade {mediaidade}')
+print(f'Homem mais velho tem {maioridadehomem} e seu nome é {nomevelho}')
+print(f'Tem {totalmulher} de mulheres com menos de 20 anos.')
