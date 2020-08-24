259e38f87971ac41a59e77f3d976177216f9a034
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:27:25 -0300

    Create funcoes.py

---
+++ Curso em Video - Python/Mundo-01/Curso Python #007 - Operadores Aritméticos/funcoes.py
@@ -0,0 +1,11 @@
+import os
+
+def title():
+    print('\033[34mPreencha logo abaixo:\033[m')
+
+def result():
+    print('\033[32mO Resultado é:\033[m')
+
+def limpa():
+    os.system('cls') #limpa
+    print('')
