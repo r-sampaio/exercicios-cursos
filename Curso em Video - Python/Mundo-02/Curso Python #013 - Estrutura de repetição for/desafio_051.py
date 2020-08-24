14602b2d63f05ca4829e95a7f5ffdbe3406174b4
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:31:34 -0300

    Create desafio_051.py

---
+++ Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_051.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, 
+    # mostre os 10 primeiros termos dessa progressão.
+
+termo = int(input('Digite o termo: '))
+razao = int(input('Digite a razão: '))
+decimo = termo + (10 - 1) * razao
+for r in range(termo, decimo, razao):
+    print(r, end=' ')
+print('FIM')
