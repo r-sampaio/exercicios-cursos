68641ae821789db285dce6051b5597bdbb12cee1
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:27:56 -0300

    Create desafio_022.py

---
+++ Curso em Video - Python/Mundo-01/Curso Python #009 - Manipulando Texto/desafio_022.py
@@ -0,0 +1,13 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+    # * O nome com todas as letras maíusculas.
+    # * O nome com todas minúsculas.
+    # * Quantas letras ao todo (sem conseferar escaços).
+    # * Quantas letras tem o primeiro nome.
+
+nome = str(input('Qual o seu nome? '))
+print(nome.upper())
+print(nome.lower())
+nomeDividido = nome.split()
+print(len(nome) - nome.count(' '))
+print(len((nomeDividido[0])))
+#print(nome.find(' '))
