c215b458cc9fd222868d71423fc9c16c349be4b0
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:27:05 -0300

    Create desafio_009.py

---
+++ Curso em Video - Python/Mundo-01/Curso Python #007 - Operadores Aritméticos/desafio_009.py
@@ -0,0 +1,26 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+from funcoes import limpa, title, result
+
+def traco():
+    n = 0
+    while n <= 3:
+        print('-'*17, end='')
+        print('  ', end='')
+        n = n+1
+
+limpa()
+title()
+num = int(input('Digite um numero: '))
+limpa()
+r = 1
+traco()
+print('')
+while r <= 10:
+    print(f'| {r:<2} + {num:<2} = {(r+num):<3} |  ', end='')
+    print(f'| {r:<2} - {num:<2} = {(r-num):<3} |  ', end='')
+    print(f'| {r:<2} x {num:<2} = {(r*num):<3} |  ', end='')
+    print(f'| {r:<2} / {num:<2} = {(r/num):<3.0f} |')
+    r = r+1
+traco()
+print('\n')
