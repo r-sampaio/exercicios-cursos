bd35b79f9073ea6f09831c1521d568023067cf7d
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:19:54 -0300

    Update desafio_069.py

--- Curso em Video - Python/Mundo-02/Curso Python #015 - Interrompendo repetições while/desafio_069.py
+++ Curso em Video - Python/Mundo-02/Curso Python #015 - Interrompendo repetições while/desafio_069.py
@@ -4,7 +4,7 @@
     # B) quantos homens foram cadastrados.
     # C) quantas mulheres tem menos de 20 anos.
 
-'''
+
 h = m = i = 0
 while True:
     idade = int(input('Digite a idade: '))
@@ -20,23 +20,24 @@ while True:
     if stop == 'N':
         break
 print(f'Tem {i} pessoas com mais de 18 anos, {h} homems e {m} mulheres com menos de 20 anos.')
-'''
 
+'''
 h = m = i = 0
 while True:
     idade = int(input('Digite a idade: '))
     sexo = ' '
     while sexo not in 'MF':
         sexo = str(input('Digite o sexo [M/F]: ')).upper().strip()[0]
-        if idade > 18:
-            i += 1
-        if sexo == 'M':
-            h += 1
-        if sexo == 'F' and idade < 20:
-            m +=1
-        stop = ' '
-        while stop not in 'SN':
-            stop = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
-            if stop == 'N':
-                break
+    if idade > 18:
+        i += 1
+    if sexo == 'M':
+        h += 1
+    if sexo == 'F' and idade < 20:
+        m +=1
+    stop = ' '
+    while stop not in 'SN':
+        stop = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if stop == 'N':
+        break
 print(f'Tem {i} pessoas com mais de 18 anos, {h} homems e {m} mulheres com menos de 20 anos.')
+'''
