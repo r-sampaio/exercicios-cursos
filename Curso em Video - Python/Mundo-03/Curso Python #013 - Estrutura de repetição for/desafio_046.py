18cb4f47bdc5631f3b4757506f0c8030e3318123
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:30:55 -0300

    update

--- Curso em Video - Python/Mundo-03/Curso Python #013 - Estrutura de repetição for/desafio_046.py
+++
@@ -1,9 +0,0 @@
-# Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
-    # indo de 10 até 0, com uma pausa de 1 segundo entre eles.
-
-from time import sleep
-from emoji import emojize
-for r in range(10, 0, -1):
-    print(r)
-    sleep(1)
-print(emojize('o/ :fireworks:'))
--- Curso em Video - Python/Mundo-03/Curso Python #013 - Estrutura de repetição for/desafio_047.py
+++
@@ -1,6 +0,0 @@
-# Crie um programa que mostre na tela todos os números pares que estão no intevalo entre 1 e 50.
-
-for i in range(1, 50):
-    if i % 2 == 0:
-        print(i, end=' ')
-print('FIM')
