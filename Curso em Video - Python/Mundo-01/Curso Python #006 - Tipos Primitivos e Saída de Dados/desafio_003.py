d09bd5e47ff7aeebd14707c03128a5fa99cb6433
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:26:39 -0300

    Create desafio_003.py

---
+++ Curso em Video - Python/Mundo-01/Curso Python #006 - Tipos Primitivos e Saída de Dados/desafio_003.py
@@ -0,0 +1,13 @@
+# Crie um script Python que leia dois número e tente mostrar a soma entre eles.
+
+from funcoes import limpa, title,result
+
+limpa()
+title()
+n1 = int(input('Digite um numero: '))
+limpa()
+title()
+n2 = int(input('Digite outro numero: '))
+limpa()
+result()
+print(f'A soma de \033[33m{n1}\033[m mais \033[32m{n2:}\033[m é igual á: \033[34m{n1 + n2}\033[m\n')
