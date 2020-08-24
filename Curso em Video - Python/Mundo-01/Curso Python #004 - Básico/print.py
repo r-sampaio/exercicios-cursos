df7d15c66f1289d4fa103cea14b8c2c524a6a2b9
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:26:33 -0300

    Create print.py

---
+++ Curso em Video - Python/Mundo-01/Curso Python #004 - Básico/print.py
@@ -0,0 +1,16 @@
+print('Hello, World!')
+
+print (7+4)
+
+print ('7' + '4')
+
+7+4
+
+'7' + '4'
+
+print ('Hello', 5, 'World!')
+
+
+print('''Nessa aula, vamos aprender operações com String no Python. As principais operações que vamos
+aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com 
+replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().''')
