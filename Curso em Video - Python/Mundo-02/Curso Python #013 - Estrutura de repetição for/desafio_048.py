95c9b5483c84f7c59bb646b0c69cb732070a4d9a
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-24 00:31:29 -0300

    Create desafio_048.py

---
+++ Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_048.py
@@ -0,0 +1,9 @@
+# Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três
+    # e que se encontram no intervalo de 1 até 500.
+s = 0
+c = 0
+for i in range(1, 501, 2):
+    if i % 3 == 0:
+        c += 1
+        s += i
+print(f'Soma {s}, quantidade de numero somados {c}')
