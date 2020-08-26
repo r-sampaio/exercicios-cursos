c61b70c44326273bc215efd18666e04d183aadb0
Rodrigo <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-25 21:24:45 -0300

    Update desafio_071.py

--- Curso em Video - Python/Mundo-02/Curso Python #015 - Interrompendo repetições while/desafio_071.py
+++ Curso em Video - Python/Mundo-02/Curso Python #015 - Interrompendo repetições while/desafio_071.py
@@ -3,3 +3,23 @@
     # quantas cédulas de cada valor serão entregues. OBS:
     # considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 
+saque = int(input('Digite o valor do saque: '))
+total = saque
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'{totalced} de R${ced}.')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
