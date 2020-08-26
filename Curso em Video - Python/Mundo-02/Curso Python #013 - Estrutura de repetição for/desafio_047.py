d657c7357c5051dc1a29b17f31d827cab6c0878f
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-25 22:14:02 -0300

    up

--- Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_047.py
+++ Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_047.py
@@ -1,6 +1,9 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intevalo entre 1 e 50.
+<<<<<<< HEAD:Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_047.py
 
 for i in range(1, 50):
     if i % 2 == 0:
         print(i, end=' ')
 print('FIM')
+=======
+>>>>>>> parent of 643e711... update:Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_047.py
--- Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_048.py
+++ Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_048.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Curso em Video - Python/Mundo-02/Curso Python #013 - Estrutura de repetição for/desafio_048.py
 # Faça um programa que calcule a soma entre todos os números ímpares que são múltiplos de três
     # e que se encontram no intervalo de 1 até 500.
 s = 0
@@ -7,3 +8,5 @@ for i in range(1, 501, 2):
         c += 1
         s += i
 print(f'Soma {s}, quantidade de numero somados {c}')
+=======
+>>>>>>> parent of 643e711... update:Curso em Video - Python/Mundo-01/Curso Python #013 - Estrutura de repetição for/desafio_048.py
