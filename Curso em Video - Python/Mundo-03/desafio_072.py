6e0680dad07d17271e1f816e1a0a13da9096bee6
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-27 22:19:45 -0300

    Update desafio_072.py

---
+++ Curso em Video - Python/Mundo-03/desafio_072.py
@@ -0,0 +1,8 @@
+# Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+    # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numero = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+num = int(input('Digite um numero: '))
+
+print(numero[num])
