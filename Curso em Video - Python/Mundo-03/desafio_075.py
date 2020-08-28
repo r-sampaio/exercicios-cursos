1760f1e77364ebbdbe723609871dd4414d29369c
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-28 00:04:08 -0300

    Update desafio_075.py

--- Curso em Video - Python/Mundo-03/desafio_075.py
+++ Curso em Video - Python/Mundo-03/desafio_075.py
@@ -3,19 +3,17 @@
     # B) Em que posição foi digitado o primeiro valor 3.
     # C) Quais foram os números pares. 
 
-n = q = r = p = 0
-while n <= 3:
-    num = (int(input('Digite um numero: '))
-    n += 1
+num = int(input('Digite um numero: ')), int(input('Digite um numero: ')), int(input('Digite um numero: ')), int(input('Digite um numero: '))
 
-if i in num:
-    if i == 9:
-        q += 1
-    if i == 3:
-        print(f'O numero 3 aparece na posição {num.index(3)}')
-    else:
-        print('O numero 3 não aparece.')
+print(f'Os numeros digitados foram,', end=' ')
+for r in num:
+    print(r, end=' ')
+print(f'\nO numero 9 apareceu {num.count(9)} vezes')
+if 3 in num:
+    print(f'O numero 3 aparece na posição {num.index(3)+1}')
+else:
+    print('Não foi digitado o numero 3.')
+print(f'Os numeros pares digitados foram', end=' ')
+for i in num:
     if i % 2 == 0:
-        r = i
-        p += 1
-
+        print(i, end=' ')
