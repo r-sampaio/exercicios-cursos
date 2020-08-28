1ae2b2cacbe522466ffc24a500515317cc36dae9
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-27 23:53:28 -0300

    Update desafio_075.py

---
+++ Curso em Video - Python/Mundo-03/desafio_075.py
@@ -0,0 +1,21 @@
+# Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+    # A) Quantas vezes apareceu o valor 9.
+    # B) Em que posição foi digitado o primeiro valor 3.
+    # C) Quais foram os números pares. 
+
+n = q = r = p = 0
+while n <= 3:
+    num = (int(input('Digite um numero: '))
+    n += 1
+
+if i in num:
+    if i == 9:
+        q += 1
+    if i == 3:
+        print(f'O numero 3 aparece na posição {num.index(3)}')
+    else:
+        print('O numero 3 não aparece.')
+    if i % 2 == 0:
+        r = i
+        p += 1
+
