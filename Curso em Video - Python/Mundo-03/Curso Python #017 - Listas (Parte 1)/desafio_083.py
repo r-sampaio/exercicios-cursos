f2c7761a04ebf5065364a06534a5623579a63d39
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-29 16:03:41 -0300

    Update desafio_083.py

--- Curso em Video - Python/Mundo-03/Curso Python #017 - Listas (Parte 1)/desafio_083.py
+++ Curso em Video - Python/Mundo-03/Curso Python #017 - Listas (Parte 1)/desafio_083.py
@@ -4,11 +4,18 @@
 
 p = 0
 lista = []
-lista.append(str(input('Digite a expressão: ')))
-for i in lista:
-    if i == '(' or i == ')':
-        p += 1
-if p % 2 == 0:
+r = []
+expr = (str(input('Digite a expressão: ')))
+for i in expr:
+    if i == '(':
+        r.append('(')
+    elif i == ')':
+        if len(r) > 0:
+            r.pop()
+        else:
+            r.append(')')
+            break
+if len(r) == 0:
     print('A expressão esta correta.')
 else:
     print('A expressão não esta correta.')
