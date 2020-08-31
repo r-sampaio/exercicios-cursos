3cf4ab237680926cd6437c15f3025f5b9000afa8
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-30 21:43:59 -0300

    Create 10.py

---
+++ ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/10.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+celsius = float(input('Digite a temperatura em Celsius: '))
+fahrenheit = (celsius * 9/5) + 32
+print(f'A temperatura de {celsius}°C equivale a aproximadamente {fahrenheit:.2f}°F.')
