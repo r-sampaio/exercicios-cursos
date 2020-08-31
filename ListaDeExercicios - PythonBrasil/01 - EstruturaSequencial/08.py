a5cb788ca1303d1d6992046531331ed22008a5c2
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-30 21:43:56 -0300

    Update 08.py

--- ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/08.py
+++ ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/08.py
@@ -1,3 +1,7 @@
 # Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
 # Calcule e mostre o total do seu salário no referido mês.
 
+valor = float(input('Digite quanto você quer ganhar por horas trabalhadas: R$'))
+horas = float(input('Digite quantas horas você trabalhou esse mês: '))
+toral = valor * horas
+print(f'Você receberá o valor de R${toral} por seu trabalho.')
