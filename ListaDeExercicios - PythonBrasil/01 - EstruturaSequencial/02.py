cf8d5f0dad87dd9926128ead7bf6063be85c7afb
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-30 22:12:57 -0300

    Update 02.py

--- ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/02.py
+++ ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/02.py
@@ -1,3 +1,3 @@
 # Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 numero = int(input('Digite um número: '))
-print(numero)
+print(f'O número informado foi {numero}')
