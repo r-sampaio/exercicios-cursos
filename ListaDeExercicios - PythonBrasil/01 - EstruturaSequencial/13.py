9af3864be931d949609e5656bd1216c9642416f1
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-08-30 22:03:46 -0300

    Update 13.py

--- ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/13.py
+++ ListaDeExercicios - PythonBrasil/01 - EstruturaSequencial/13.py
@@ -3,3 +3,11 @@
     # Para homens: (72.7*h) - 58
     # Para mulheres: (62.1*h) - 44.7
 
+sexo = str(input('Digite sue sexo [M/F]: ')).strip()[0]
+if sexo in 'Mm':
+    altura = float(input('Digite sua altura: '))
+    result = (72.7*altura) - 58
+elif sexo in 'Ff':
+    altura = float(input('Digite sua altura: '))
+    result = (62.1*altura) - 44.7
+print(f'Seu peso ideal é {result:.2f}kg.')
