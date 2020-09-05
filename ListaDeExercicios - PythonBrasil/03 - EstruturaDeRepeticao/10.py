9cc03d9263b7a99ae72eea1708a53c651bdcbb5a
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-05 16:41:33 -0300

    Update 10.py

--- ListaDeExercicios - PythonBrasil/03 - EstruturaDeRepeticao/10.py
+++ ListaDeExercicios - PythonBrasil/03 - EstruturaDeRepeticao/10.py
@@ -5,5 +5,9 @@ que estão no intervalo compreendido por eles.
 
 n1 = int(input('Digite um inteiro: '))
 n2 = int(input('Digite outro inteiro: '))
-for i in range(n1 +1 , n2):
-    print(f'{i} ', end='')
+if n1 < n2:
+    for i in range(n1 +1 , n2):
+        print(f'{i} ', end='')
+else:
+    for i in range(n1 -1, n2, -1):
+        print(f'{i} ', end='')
