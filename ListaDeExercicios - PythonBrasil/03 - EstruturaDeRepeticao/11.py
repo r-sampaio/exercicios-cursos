cfabb6519a8ce7aa7612810394ad0992a23274f7
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-05 16:38:44 -0300

    Create 11.py

---
+++ ListaDeExercicios - PythonBrasil/03 - EstruturaDeRepeticao/11.py
@@ -0,0 +1,14 @@
+# Altere o programa anterior para mostrar no final a soma dos números.
+
+soma = 0
+n1 = int(input('Digite um inteiro: '))
+n2 = int(input('Digite outro inteiro: '))
+if n1 < n2:
+    for i in range(n1 +1 , n2):
+        print(f'{i} ', end='')
+        soma += i
+else:
+    for i in range(n1 +1 , n2, -1):
+        print(f'{i} ', end='')
+        soma += i
+print(f'\nSomados dão o valor de: {soma}')
