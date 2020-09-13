3ae351c0042b80218230c3834e61f861a48e3c41
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 20:36:34 -0300

    Create teste.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #022 - Módulos e Pacotes/desafio_107/teste.py
@@ -0,0 +1,14 @@
+# Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() 
+# e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
+
+# from moeda import dobro, metade, aumentar, diminuir
+# # import moeda
+# # from desafio_107 import moeda
+# # import desafio_107.moeda
+
+# p = float(input('Digite um valor: R$ '))
+# print()
+# print(f'Um aumento de 10% de R$ {p} vai para R$ {aumentar(p, 10)}')
+# print(f'Uma redução de 13% de R$ {p} vai para R$ {diminuir(p, 13)}')
+# print(f'O dobro de R$ {p} é R$ {dobro(p)}')
+# print(f'A metade de R$ {p} é R$ {metade(p)}')
