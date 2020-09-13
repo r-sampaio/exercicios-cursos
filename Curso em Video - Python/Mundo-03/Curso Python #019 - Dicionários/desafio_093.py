9611ba3ca3da2b726817890ec6c74e734e4842d9
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 18:23:41 -0300

    Update desafio_093.py

--- Curso em Video - Python/Mundo-03/Curso Python #019 - Dicionários/desafio_093.py
+++ Curso em Video - Python/Mundo-03/Curso Python #019 - Dicionários/desafio_093.py
@@ -4,21 +4,21 @@
 # será guardado em um dicionário, incluindo o total de gols feitos durante 
 # o campeonato.
 
-jogador = dict()
-partidas = list()
-jogador['nome'] = str(input('Nome do jogador: '))
-tot = int(input(f'Quantas partidas {jogador["nome"]} jogou. '))
-for c in range(0, tot):
-    partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
-jogador['gols'] = partidas[:]
-jogador['total'] = sum(partidas)
-print()
-print(jogador)
-print()
-for k, v in jogador.items():
-    print(f'O Campo {k} tem o valor {v}')
-print()
-print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
-for i, v in enumerate(jogador["gols"]):
-    print(f' Na partida {i+1}, fez {v} gols.')
-print(f'Foi um total de {jogador["total"]} gols.')
+# jogador = dict()
+# partidas = list()
+# jogador['nome'] = str(input('Nome do jogador: '))
+# tot = int(input(f'Quantas partidas {jogador["nome"]} jogou. '))
+# for c in range(0, tot):
+#     partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+# jogador['gols'] = partidas[:]
+# jogador['total'] = sum(partidas)
+# print()
+# print(jogador)
+# print()
+# for k, v in jogador.items():
+#     print(f'O Campo {k} tem o valor {v}')
+# print()
+# print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+# for i, v in enumerate(jogador["gols"]):
+#     print(f' Na partida {i+1}, fez {v} gols.')
+# print(f'Foi um total de {jogador["total"]} gols.')
