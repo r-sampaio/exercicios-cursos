2807f2580129fc2412f2ae15270c11795c81f46b
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 18:23:52 -0300

    Update desafio_102.py

--- Curso em Video - Python/Mundo-03/Curso Python #021 - Funções (Parte 2)/desafio_102.py
+++ Curso em Video - Python/Mundo-03/Curso Python #021 - Funções (Parte 2)/desafio_102.py
@@ -2,24 +2,24 @@
 # o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será 
 # mostrado ou não na tela o processo de cálculo do fatorial.
 
-def fatorial(n, show=False):
-    """
-    -> Calcula o fatorial de um número.
-    :param n: número a ser calculado.
-    :param show: (opcional) mostrar ou não a conta.
-    :return: o valor do fatorial de um número n.
-    """
-    f = 1
-    for c in range(n, 0, -1):
-        if show:
-            print(c, end='')
-            if c > 1:
-                print(' x ', end='')
-            else:
-                print(' = ', end='')
-        f *= c
-    return f
+# def fatorial(n, show=False):
+#     """
+#     -> Calcula o fatorial de um número.
+#     :param n: número a ser calculado.
+#     :param show: (opcional) mostrar ou não a conta.
+#     :return: o valor do fatorial de um número n.
+#     """
+#     f = 1
+#     for c in range(n, 0, -1):
+#         if show:
+#             print(c, end='')
+#             if c > 1:
+#                 print(' x ', end='')
+#             else:
+#                 print(' = ', end='')
+#         f *= c
+#     return f
 
-# programa principal
-print(fatorial(5, show=True))
-help(fatorial)
+# # programa principal
+# print(fatorial(5, show=True))
+# help(fatorial)
