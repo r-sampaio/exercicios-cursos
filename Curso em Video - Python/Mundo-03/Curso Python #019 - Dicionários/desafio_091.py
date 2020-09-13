7f82da00d310824c5fe7e73d2e9991ae4c126cd3
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 18:23:36 -0300

    Update desafio_091.py

--- Curso em Video - Python/Mundo-03/Curso Python #019 - Dicionários/desafio_091.py
+++ Curso em Video - Python/Mundo-03/Curso Python #019 - Dicionários/desafio_091.py
@@ -2,19 +2,19 @@
 # Guarde esses resultados em um dicionário em Python. No final, coloque esse 
 # dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
-from random import randint
-from time import sleep
-from operator import itemgetter
+# from random import randint
+# from time import sleep
+# from operator import itemgetter
 
-numero = {'jogador1': randint(1, 6),
-          'jogador2': randint(1, 6),
-          'jogador3': randint(1, 6),
-          'jogador4': randint(1, 6)}
-ranking = dict()
-for k, v in numero.items():
-    print(f'{k} jogou {v}')
-    sleep(1)
-ranking = sorted(numero.items(), key=itemgetter(1), reverse=True)
-for i, v in enumerate(ranking):
-    print(f'{i+1} lugar: {v[0]} com {v[1]}')
-    sleep(1)
+# numero = {'jogador1': randint(1, 6),
+#           'jogador2': randint(1, 6),
+#           'jogador3': randint(1, 6),
+#           'jogador4': randint(1, 6)}
+# ranking = dict()
+# for k, v in numero.items():
+#     print(f'{k} jogou {v}')
+#     sleep(1)
+# ranking = sorted(numero.items(), key=itemgetter(1), reverse=True)
+# for i, v in enumerate(ranking):
+#     print(f'{i+1} lugar: {v[0]} com {v[1]}')
+#     sleep(1)
