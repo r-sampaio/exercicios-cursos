a6df6cb09c8bfe7900d25a9f895cf00ef12e07cb
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 18:25:47 -0300

    Create desafio_103.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #021 - Funções (Parte 2)/desafio_103.py
@@ -0,0 +1,21 @@
+# Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: 
+# o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha 
+# do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+
+# def ficha(jog = '<desconhecido>', gol = 0):
+#     print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
+
+
+# # programa principal
+# nome = str(input('Nome do Jogador: '))
+# gols = str(input('Número de Gols: '))
+
+# if gols.isnumeric():
+#     gols = int(gols)
+# else:
+#     gols = 0
+# if nome.strip() == '':
+#     ficha(gol = gols)
+# else:
+#     ficha(nome, gols)
