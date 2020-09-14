bbca1f4fbfaf40f768031f46b195bac3b4a625b8
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 21:40:47 -0300

    Create teste.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #022 - Módulos e Pacotes/desafio_108/teste.py
@@ -0,0 +1,11 @@
+# Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga 
+# mostrar os números como um valor monetário formatado.
+
+# from desafio_108 import moeda
+
+# p = float(input('Digite um valor: R$ '))
+# print()
+# print(f'Um aumento de 10% de R$ {moeda.moeda(p)} vai para R$ {moeda.moeda(moeda.aumentar(p, 10))}')
+# print(f'Uma redução de 13% de R$ {moeda.moeda(p)} vai para R$ {moeda.moeda(moeda.diminuir(p, 13))}')
+# print(f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.dobro(p))}')
+# print(f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.moeda(moeda.metade(p))}')
