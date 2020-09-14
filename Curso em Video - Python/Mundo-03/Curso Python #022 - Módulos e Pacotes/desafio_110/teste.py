6113e57907d2fe6489179093791d5205721274ba
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 21:58:13 -0300

    Create teste.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #022 - Módulos e Pacotes/desafio_110/teste.py
@@ -0,0 +1,9 @@
+# Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mostre 
+# na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+
+
+# from desafio_110 import moeda
+
+# p = float(input('Digite um valor: R$ '))
+# print()
+# moeda.resumo(p, 35, 12)
