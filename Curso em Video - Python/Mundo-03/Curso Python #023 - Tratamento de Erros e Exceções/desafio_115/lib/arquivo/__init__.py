1f9dfc879bd16701c22b037f9b92bc830b58c958
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 23:45:45 -0300

    Create __init__.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #023 - Tratamento de Erros e Exceções/desafio_115/lib/arquivo/__init__.py
@@ -0,0 +1,51 @@
+from interface import *
+
+
+def arqExiste(nome):
+    try:
+        a = open(nome, 'rt') #le
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')  #cria write text +
+        a.close()
+    except:
+        print('\033[31m.Houve um ERRO na criação do arquivo!\033[m')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo')
+    else:
+        cabecalho('\033[36mPESSOAS REGISTRADAS\033[m')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n','')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='desconhecido', idade=0):
+    try:
+        a = open(arq, 'at') #adicionar a de append
+    except:
+        print('Houve um ERRO na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um ERRO na hora de escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado')
+            a.close()
