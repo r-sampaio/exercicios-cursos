7e1d59242ed20a168e19b0c6e822c74060d08d4d
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 22:13:15 -0300

    Create teste.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #022 - Módulos e Pacotes/desafio_111/teste.py
@@ -0,0 +1,11 @@
+# Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado. 
+# Transfira todas as funções utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e 
+# mantenha tudo funcionando.
+
+
+# from desafio_111.utilidadesCeV import moeda
+
+
+# p = float(input('Digite um valor: R$ '))
+# print()
+# moeda.resumo(p, 35, 12)
