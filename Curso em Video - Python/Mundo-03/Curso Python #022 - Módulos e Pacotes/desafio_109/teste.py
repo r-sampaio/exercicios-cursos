150f172e2e8a58f15d4b194c86ad9dd38f048df8
rodrigosampai0 <16281210+rodrigosampai0@users.noreply.github.com>
2020-09-13 21:51:36 -0300

    Create teste.py

---
+++ Curso em Video - Python/Mundo-03/Curso Python #022 - Módulos e Pacotes/desafio_109/teste.py
@@ -0,0 +1,13 @@
+# Modifique as funções que form criadas no desafio 107 para que elas aceitem um 
+# parâmetro a mais, informando se o valor retornado por elas vai ser ou não 
+# formatado pela função moeda(), desenvolvida no desafio 108.
+
+
+# from desafio_109 import moeda
+
+# p = float(input('Digite um valor: R$ '))
+# print()
+# print(f'Um aumento de 10% de R$ {moeda.moeda(p)} vai para R$ {moeda.aumentar(p, 10, True)}')
+# print(f'Uma redução de 13% de R$ {moeda.moeda(p)} vai para R$ {moeda.diminuir(p, 13, True)}')
+# print(f'O dobro de R$ {moeda.moeda(p)} é R$ {moeda.dobro(p, True)}')
+# print(f'A metade de R$ {moeda.moeda(p)} é R$ {moeda.metade(p, True)}')
